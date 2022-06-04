9446fd8f10b749eee74309ad2492b525165a53e8
Vladimir Stupin <vladimir@stupin.su>
2022-06-04 18:34:33 +0500

    При сборке программы из неё удаляются неиспользуемые функции и данные

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@
 rm test
 gcc bin.c test.c -o test -std=c99 -Wpedantic -Wall -Wextra
 ./test
-gcc bin.c restable.c grp.c gob.c pak.c viv.c vpp.c pack.c pack2.c wad23.c res.c dpk4.c dat.c dat2.c rff.c labn.c meta.c c_zlib.c c_pack2.c c_lzss.c c_dat.c comdec.c main.c -D_FILE_OFFSET_BITS=64 -I/usr/include/ -L/usr/lib/ -lz -std=c99 -Wpedantic -Wall -Wextra -o pup # -g
+gcc bin.c restable.c grp.c gob.c pak.c viv.c vpp.c pack.c pack2.c wad23.c res.c dpk4.c dat.c dat2.c rff.c labn.c meta.c c_zlib.c c_pack2.c c_lzss.c c_dat.c comdec.c main.c -D_FILE_OFFSET_BITS=64 -I/usr/include/ -L/usr/lib/ -lz -std=c99 -Wpedantic -Wall -Wextra -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--print-gc-sections -Wl,-s -o pup # -g
